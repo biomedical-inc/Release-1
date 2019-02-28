5f268c5c29d1530dd731defa377724c56b2cfcf7
2651021-Ben <47429983+2651021-Ben@users.noreply.github.com>
2019-02-28 14:20:56 -0500

    suite de synchroniser
    synchroniser

--- Synchronisé.php
+++ Synchronisé.php
@@ -2,15 +2,34 @@
 	include(../PersonneDAO.php);
 	include(../PersonneVO.php);
 	
-	protected $dao = new DAO();
+	protected $dao = new PersonneDAO();
+	protected $utilisateur = new PersonneVO()
 	
 	public function Synchroniser(PersonneVO personne){
 		//si la personne existe insere son info
-		if(personne.identifiant == $dao.getPersonne(personne.identifiant)){
-			$dao.InsertPersonne(personne.DPrsTst,personne.PrsnSys,personne.PrsnDiasys,personne.PlCard);
+		if(personne.getId() == $dao.getPersonne(personne.getId())){
+		$utilisateur.setPrenom(personne.getPrenom());
+			$utilisateur.setNom(personne.getNom());
+			$utilisateur.setID(personne.getId());
+			$utilisateur.setPw(personne.getPw());
+			$utilisateur.setDateNaissance(personne.getDateNaissance());
+			$utilisateur.setDatePriseTest(personne.getDatePriseTest());
+			$utilisateur.setPressionSys(personne.getPressionSys());
+			$utilisateur.setPressionDiaSys(personne.getPressionDiaSys());
+			$utilisateur.setPoulCardiaque(personne.getPoulCardiaque());
+			$dao.Synchroniser($utilisateur);
 		}
 		else{
-			$dao.CreerPersonne(personne.identifiant,personne.Pwd)
+			$utilisateur.setPrenom(personne.getPrenom());
+			$utilisateur.setNom(personne.getNom());
+			$utilisateur.setID(personne.getId());
+			$utilisateur.setPw(personne.getPw());
+			$utilisateur.setDateNaissance(personne.getDateNaissance());
+			$utilisateur.setDatePriseTest(personne.getDatePriseTest());
+			$utilisateur.setPressionSys(personne.getPressionSys());
+			$utilisateur.setPressionDiaSys(personne.getPressionDiaSys());
+			$utilisateur.setPoulCardiaque(personne.getPoulCardiaque());
+			$dao.CreerPersonne($utilisateur)
 		}
 	}
 ?>
